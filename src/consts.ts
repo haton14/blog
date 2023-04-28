e650584f150d617db1b69a27da426849c5f4503f
haton14 <iioka.takumi.quena@gmail.com>
2023-04-28 17:49:02 +0900

    最初の記事

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ソフトウェア開発者の語り';
+export const SITE_DESCRIPTION = 'haton14のブログ';
